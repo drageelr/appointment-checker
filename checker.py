ab586140b0c72b010636cbc5c066399256b4eabe
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-09-16 21:35:05 +0500

    Added print statements

--- checker.py
+++ checker.py
@@ -10,30 +10,23 @@ def check():
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
     driver = webdriver.Chrome(options=op)
-    # print('Driver Started')
-    driver.get("https://service2.diplo.de/rktermin/extern/choose_realmList.do?locationCode=isla&request_locale=en")
-    # print('Page Fetched')
+    print('Driver Started')
+    driver.get("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600")
+    print('Page Fetched')
     try:
-        btn1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/div[1]/div[1]/div[3]/a")))
-        # print('Btn 1 Fetched')
-        driver.execute_script("arguments[0].click();", btn1)
-        btn2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/div[1]/div[1]/div[12]/a")))
-        # print('Btn 2 Fetched')
-        driver.execute_script("arguments[0].click();", btn2)
-        btn3 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/h3[2]/a[2]")))
-        # print('Btn 3 Fetched')
-        driver.execute_script("arguments[0].click();", btn3)
         elem_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
-        # print('Select Element Fetched')
+        print('Select Element Fetched')
         
         option_elems = elem_select.find_elements(By.TAG_NAME ,"option")
-        # print('Options Fetched')
+        print('Options Fetched')
         active = False
         active_value = ''
         for opt in option_elems:
+            print(opt, os.environ.get('KEYWORD'))
             opt_value = opt.get_attribute("value")
             if isinstance(opt_value, str) and os.environ.get('KEYWORD') in opt_value.lower():
                 active = True
+                print('Found')
                 # active_value = opt_value
                 break
             
@@ -47,7 +40,7 @@ def check():
             driver.quit()
             return (None, False)
     except Exception as e:
-        print('ERROR', e)
+        print('ERROR', e.__name__)
         driver.quit()
         # print(e)
         return (e, False)
