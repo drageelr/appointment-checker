9058b64dd5a3b214f32793869cddd071cbca5edc
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-09-16 22:27:53 +0500

    Added user agent

--- checker.py
+++ checker.py
@@ -7,15 +7,24 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def check():
     
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(options=op)
+    chrome_options = webdriver.ChromeOptions()
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    chrome_options.add_argument(f'user-agent={user_agent}')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
     print('Driver Started')
     driver.get("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600")
     print('Page Fetched')
     print(driver.page_source)
     try:
-        elem_select = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
+        elem_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
         print('Select Element Fetched')
 
         option_elems = elem_select.find_elements(By.TAG_NAME ,"option")
