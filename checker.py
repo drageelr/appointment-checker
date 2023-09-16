d1f052ef5d5dfea7a45794f4a561bc68a2ad725c
Hammad Nasir <hammadn99@gmail.com>
2023-09-16 21:07:24 +0500

    Update checker.py
    os import missing

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 
 def check():
     
