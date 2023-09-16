d924fbebc9acd205360bceed9e6247e10e8ba913
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-09-16 23:04:21 +0500

    Refactored checker

--- checker.py
+++ checker.py
@@ -7,32 +7,33 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def check():
     
-    chrome_options = webdriver.ChromeOptions()
+    op = webdriver.ChromeOptions()
 
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    chrome_options.add_argument("--headless")
+    
+    op.add_argument(f'user-agent={user_agent}')
+    op.add_argument('--no-sandbox')
+    op.add_argument('--window-size=1920,1080')
+    op.add_argument('--headless')
+    op.add_argument('--disable-gpu')
+    op.add_argument('--allow-running-insecure-content')
+    op.add_argument('--headless')
 
-    driver = webdriver.Chrome(options=chrome_options)
-    print('Driver Started')
+    driver = webdriver.Chrome(options=op)
+    # print('Driver Started')
     driver.get("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600")
-    print('Page Fetched')
-    print(driver.page_source)
+    # print('Page Fetched')
+
     try:
         elem_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
-        print('Select Element Fetched')
+        # print('Select Element Fetched')
 
         option_elems = elem_select.find_elements(By.TAG_NAME ,"option")
-        print('Options Fetched')
+        # print('Options Fetched')
+
         active = False
         active_value = ''
         for opt in option_elems:
-            print(opt, os.environ.get('KEYWORD'))
             opt_value = opt.get_attribute("value")
             if isinstance(opt_value, str) and os.environ.get('KEYWORD') in opt_value.lower():
                 active = True
@@ -50,7 +51,7 @@ def check():
             driver.quit()
             return (None, False)
     except Exception as e:
-        print('ERROR', e.__name__)
+        print('ERROR')
         driver.quit()
         # print(e)
         return (e, False)
