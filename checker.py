61de52826affb11f0a966c37c845e391a6e109d4
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-11-13 19:11:38 +0500

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,21 +1,25 @@
 import os
 import logging
 
-import undetected_chromedriver as UC
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def check():
-    options = webdriver.ChromeOptions() 
-    options.add_argument("start-maximized")
-    driver = uc.Chrome(options=options)
-    print('Driver Started')
     
-    driver.get("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600")
-    print('Page Fetched')
+    op = webdriver.ChromeOptions()
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    
+    op.add_argument(f'user-agent={user_agent}')
+    op.add_argument('--no-sandbox')
+    op.add_argument('--window-size=1920,1080')
+    op.add_argument('--disable-gpu')
+    op.add_argument('--allow-running-insecure-content')
+    op.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=op)
 
     try:
         elem_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
